cef735e6017521928e8bf37ce325517b89d2df46
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-10 21:02:07 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240110/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    double a, b;
+    a=13;
+    b=0.165;
+
+    cout << fixed;
+
+    cout.precision(6);
+
+    cout << "13 * 0.165000 = " << a*b;
+
+    
+    return 0;
+}
