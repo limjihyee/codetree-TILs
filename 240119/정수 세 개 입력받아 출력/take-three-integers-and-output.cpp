83d52e4fa4edb0b85fee5a98f03eedfda85f8ff6
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-19 19:39:20 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if(1<=a<=100 && 1<=b<=100 && 1<=c<=100){
+        cout << a << " " << b  << " " << c;
+    }
+    return 0;
+}
