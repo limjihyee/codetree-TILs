4920fa36feb3a09995925ee30ab095b5891818e1
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-19 18:30:21 +0900

    Title: 입력받아 계산, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/입력받아 계산/input-calculate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+
+    if(1 <= a <= 100){
+        cin >> a;
+
+        cout << a+2;
+    }
+    else cout << "범위 안에서 출력해주세요";
+
+    return 0;
+}
