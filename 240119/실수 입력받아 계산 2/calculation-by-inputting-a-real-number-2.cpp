766edf06de6cbbec57d355e31d4abceaf7b94716
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-19 18:51:41 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a;
+    cin >> a;
+
+    if(1<=a<=100){
+        cout << fixed;
+        cout.precision(2);
+
+        cout << a+1.5;
+    }
+    return 0;
+}
