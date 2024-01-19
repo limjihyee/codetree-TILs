de49d543ceac2d40572795d8bed52433541f940d
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-19 19:52:30 +0900

    Title: 문자열 받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240119/문자열 받아 출력/enter-word-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string s;
+    int len = s.length();
+    cin >> s;
+    if(1<=len<=100){
+        cout << s;
+    }
+    return 0;
+}
