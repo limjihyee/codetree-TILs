7a77b5ae94cf7c8c8ec4a42565cbaff4b281c375
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-19 18:23:56 +0900

    Title: 변수 값 복사하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240119/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    c=a=b;
+
+    cout << a << " " << b << " " << c;
+    return 0;
+}
