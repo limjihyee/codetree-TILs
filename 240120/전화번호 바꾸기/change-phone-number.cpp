b94ee6d82b2454f177e51341c05f01a950dfb681
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-20 17:09:41 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// c언어에서 정수형 변수는 0으로 시작하는 숫자를 인식하지 못해서 010 입력하면 10이 저장됨
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int c, xxxx, yyyy;
+    char d;
+
+    cin >> c >> d >> xxxx >> d >> yyyy;
+
+    cout << "0" << c << d << yyyy << d << xxxx;
+
+    return 0;
+}
