657367a37f0a0c90639c60a0d461bd5bbd38c222
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-22 22:05:58 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240122/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    cin >> a >> b;
+
+    c = a > b ? a : b;
+
+    cout << c;
+    
+    return 0;
+}
