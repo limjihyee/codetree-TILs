79ef3671bf766574e6394523ecd75b8a809bb484
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-23 19:29:17 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    string b;
+
+    cin >> a;
+
+    b = a == 100 ? "pass" : "fail";
+
+    cout << b;
+
+    return 0;
+}
