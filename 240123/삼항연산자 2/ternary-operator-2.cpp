d3ed2a6e596ff3fe89c0857ea1cabdcf42b5ebf6
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-23 19:31:48 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    char b;
+
+    cin >> a;
+
+    b = a == 1 ? 't' : 'f';
+
+    cout << b;
+    return 0;
+}
