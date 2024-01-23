bb90072edd0458afb6ea68a11ee4b64e002a7b21
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-24 00:33:16 +0900

    Title: 시력 검사, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/시력 검사/eye-test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    cin >> a >> b;
+
+    if(a>=1.0 && b>=1.0){
+        cout << "High";
+    }
+    else if(a>=0.5 && b>=0.5){
+        cout << "Middle";
+    }
+    else cout << "Low";
+
+    return 0;
+}
