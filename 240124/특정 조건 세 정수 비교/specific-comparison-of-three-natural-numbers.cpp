de8d2a781fe814af496d1f53c1350314f0e01a7c
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-24 01:43:57 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int d;
+    if(a<b && a<c){ // a가 제일 작을 경우
+        d = a;
+    }
+    else if(b<a && b<c){ // b가 제일 작을 경우
+        d = b;
+    }
+    else if(c<a && c<b) {
+        d = c;
+    }
+
+    if(a==d){
+        cout << 1 << " ";
+    }
+    else cout << 0 << " ";
+
+    if(a==b && a==c){
+        cout << 1;
+    }
+    else cout << 0;
+
+    return 0;
+}
