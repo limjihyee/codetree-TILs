7a9ba7dea848fa19b70a3f5f9c12bdc6f064fab5
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-25 15:53:53 +0900

    Title: 짝수만 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int b, a;
+
+    cin >> b >> a;
+
+    int i = b;
+    while(i>=a){
+        if(i%2==0){
+            cout << i << " ";
+        }
+        i--;
+    }
+    
+    return 0;
+}
