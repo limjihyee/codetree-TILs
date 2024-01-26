6800d0c7f07b5cfc6380ed73f170826ed42ba8b0
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 02:57:39 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int cnt=0;
+
+    for(int i=a; i<=b; i++){
+        if(i%c!=0){ // c의 배수가 없다면
+            cnt++;
+        }
+    }
+
+    if(cnt>0){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
+    }
+    
+    return 0;
+}
