da666d16479bef986b1546eb1f31e8d47aee9873
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 03:33:13 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/소수 판별기/prime-number-discriminator.cpp
+++ 240127/소수 판별기/prime-number-discriminator.cpp
@@ -7,7 +7,7 @@ int main() {
 
     int cnt = 0;
 
-    for(int i=2; i<1000; i++){
+    for(int i=2; i<1000; i++){  // 1000 말고 n으로 해도 됨 (약수를 구하는 거니까 n보다 클 수는 없음)
         if(i!=n){
             if(n%i==0){
                 cnt++;
@@ -21,6 +21,6 @@ int main() {
     else{
         cout << "P";
     }
-    
+
     return 0;
 }
