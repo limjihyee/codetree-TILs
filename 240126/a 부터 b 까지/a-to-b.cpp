0f442040a79a69751676bd29395d4506ffca6c12
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-26 16:45:17 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int i =a;
+    cout << i << " ";
+    while(i<b){
+        if(i%2==1){
+            i*=2;
+            cout << i << " ";
+        }
+        else {
+            i+=3;
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
