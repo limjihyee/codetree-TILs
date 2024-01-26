0041f2896b6f0570d1032cafc2fe07d638a805d0
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 01:26:02 +0900

    Title: 몫 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/몫 출력하기/print-share.cpp
+++ 240127/몫 출력하기/print-share.cpp
@@ -9,14 +9,14 @@ int main() {
         int k;
         cin >> k;
 
-        cnt++;
-        
-        if(cnt>3){
-            break;
-        }
-
         if(k%2==0){
             cout << k/2 << endl;
+
+            cnt++;
+
+            if(cnt==3){
+                 break;
+            }
         }
     }
     return 0;
