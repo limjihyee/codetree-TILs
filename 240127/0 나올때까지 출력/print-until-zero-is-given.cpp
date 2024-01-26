bff3b0cd292ced1a9abb72f7b4822bb3481a1e2e
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 00:48:04 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240127/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for(;;){
+        int k;
+        cin >> k;
+        
+        if(k!=0){
+        cout << k << endl;
+        }
+
+        if(k==0){
+            break;
+        }
+    }
+
+    return 0;
+}
