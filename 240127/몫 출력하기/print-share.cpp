15ed1e7d631b92e70212888539e1c9c8fc1437c3
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 01:18:09 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240127/몫 출력하기/print-share.cpp
+++ 240127/몫 출력하기/print-share.cpp
@@ -9,13 +9,14 @@ int main() {
         int k;
         cin >> k;
 
-        if(k%2==0){
-            cout << k/2 << endl;
-        }
         cnt++;
         
         if(cnt>3){
             break;
+        }
+
+        if(k%2==0){
+            cout << k/2 << endl;
         }
     }
     return 0;
