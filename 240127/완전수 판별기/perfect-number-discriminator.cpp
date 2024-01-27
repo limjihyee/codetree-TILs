7bcaa0442238959a3e9f0eae5bf1fdfb1edadd91
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-27 21:31:39 +0900

    Title: 완전수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum=0;
+    for(int i=1; i<=n; i++){
+        if(n%i==0){ // 나눠지는게 있으면 약수가 있음
+            n/=i;
+            sum += i;
+        }
+    }
+
+    if(n=sum/2){
+        cout << 'P';
+    }
+    else{
+        cout << 'N';
+    }
+
+    return 0;
+}
