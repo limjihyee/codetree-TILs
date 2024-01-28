639e844a9624ec0ae2422a54ac9d226277f74a73
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-29 00:55:19 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=i; j>0; j--){ // 공백
+            cout << "  ";
+        }
+        for(int k=-2*i+(2*n-1); k>0; k--){ // 감소하는 별
+            cout << "* ";
+        }
+       
+        // for(int l=0; l<2*i+3; l++){ // 증가하는 별
+        //     cout << "* ";
+        // }
+        cout << endl;
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int j=n-2-i; j>0; j--){ // 감소하는 공백
+            cout << "  ";
+        }
+        for(int l=0; l<2*i+3; l++){ // 증가하는 별
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
