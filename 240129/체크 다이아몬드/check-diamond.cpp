bb1f36101f4bc98d6ddbc32ba1a96fa520cdc13b
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-29 01:48:08 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int k=0; k<-i+n-1; k++){ // 증가하는 공백
+            cout << " ";
+        }
+        for(int j=0; j<i+1; j++){ // 증가하는 별
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int k=0; k<i+1; k++){ // 감소하는 공백
+            cout << " ";
+        }
+        for(int j=0; j<-i+n-1; j++){ // 감소하는 별
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
