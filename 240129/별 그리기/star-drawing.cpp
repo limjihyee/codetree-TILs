6987b140b8d603ecd8a2223be105d75f211bf030
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-29 01:20:41 +0900

    Title: 별 그리기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/별 그리기/star-drawing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int k=0; k<-i+n-1; k++){ // 증가하는 공백
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1; j++){ // 증가하는 별
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int k=0; k<i+1; k++){
+            cout << " ";
+        }
+        for(int j=0; j<-2*i+(2*n-3); j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
