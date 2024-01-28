bd5d254c7f48b0de9d665e748d980d06a745bb80
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-29 00:09:43 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240129/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=n; j>i; j--){
+            cout << "*";
+        }
+        for(int k=0; k<2*i; k++){
+            cout << " ";
+        }
+        for(int l=n; l>i; l--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
