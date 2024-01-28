a44a31c14e728f89f2dd91abb5069fca68af2441
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-28 21:15:06 +0900

    Title: 건너뛰고 별표 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240128/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=n; j>=n-i; j--){
+            cout << "*";
+        }
+        cout << endl;
+        cout << endl;
+    }
+
+    for(int i=n-2; i>=0; i--){
+        for(int j=0; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
