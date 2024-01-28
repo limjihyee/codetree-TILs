f670d74a2437c80f360836de938c81834f33718c
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-29 01:32:29 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240129/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 240129/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -5,18 +5,18 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++){ // 증가하는 공백
         for(int k=0; k<-i+n-1; k++){
             cout << "  ";
         }
-        for(int j=0; j<i+1; j++){
+        for(int j=0; j<i+1; j++){ //증가하는 @
             cout << "@ ";
         }
         cout << endl;
     }
 
     for(int i=0; i<n-1; i++){
-        for(int j=0; j<-i+n-1; j++){
+        for(int j=0; j<-i+n-1; j++){ // 감소하는 @
             cout << "@ ";
         }
         cout << endl;
