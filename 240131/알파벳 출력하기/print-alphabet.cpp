dcf5df872afee2e0c2cff10fa4a38340a8fa7373
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-01-31 20:17:15 +0900

    Title: 알파벳 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/알파벳 출력하기/print-alphabet.cpp
+++ 240131/알파벳 출력하기/print-alphabet.cpp
@@ -7,10 +7,10 @@ int main() {
 
     int k=0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<i+1; j++){
+        for(int j=0; j<i+1; j++){ // Z는 90, A가 65
             cout << (char)(65+k);
 
-            if(k==57){
+            if(k==25){
                 k=0;
             }
             else{
