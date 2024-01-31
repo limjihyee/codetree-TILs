b8116fa2038c5b566e17e5dc49b9075b6a62bea7
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-02-01 01:53:25 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240201/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -11,6 +11,13 @@ int main() {
     int sum=0;
     int cnt=0;
 
+    int i =0;
+    while(a[i]<250){
+        cnt++;
+        sum += a[i];
+        i++;
+    }
+
 	// for(int i = 0; i < 10; i++) {
     //     if(a[i] >= 250) 
 	// 		break;     // 250 이상이 되면 break로 for문 탈출
