6da125224652fc31bbea08b711cfc8aa8be8a1f4
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-02-01 20:07:29 +0900

    Title: 배열 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/배열 채우기/filling-array.cpp
+++ 240201/배열 채우기/filling-array.cpp
@@ -8,12 +8,14 @@ int main() {
     for(int i=0; i<10; i++){
         cin >> a[i];
         cnt++;
-        if(a[i]==0){
+
+        if(a[i] == 0){
+            cnt--; // 위에서 받은 0은 제거
             break;
         }
     }
 
-    for(int i=cnt-2; i>=0; i--){
+    for(int i=cnt-1; i>=0; i--){
         cout << a[i] << " ";
     }
 
