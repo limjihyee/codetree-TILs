8fcc58c4414178021cfd536b9d49e32e2d1956c3
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-02-02 12:28:32 +0900

    Title: 특정 위치의 숫자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240202/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10];
+
+    for(int i=0; i<10; i++){
+        cin >> a[i];
+    }
+
+    cout << a[2] + a[4] + a[9];
+
+    return 0;
+}
