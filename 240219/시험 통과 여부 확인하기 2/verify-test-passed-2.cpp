147db9a6f7c65183b73c2d0223c6262becbf1dca
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-02-19 00:22:53 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240219/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a;
+    cin >> a;
+
+    double sum[10]; // 10명 미만의 학생들의 점수 저장 (나중에 평균 계산해야 하니까 float)
+
+    int cnt = 0; // 통과한 사람 
+
+    for(int j=0; j<a; j++){
+        for(int i=0; i<4; i++){
+        int exam[4];
+        cin >> exam[i];
+
+        sum[j] += exam[i];
+        }
+
+        if(sum[j]/4 >= 60){
+            cout << "pass" << endl;
+            cnt++;
+        }
+        else
+            cout << "fail" << endl;
+    }
+    cout << cnt;
+
+    return 0;
+}
