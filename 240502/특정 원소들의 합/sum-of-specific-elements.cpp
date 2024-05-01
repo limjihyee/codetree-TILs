802dbdd8f764f303b7d7ce2d06530719c6074713
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-05-02 02:41:34 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240502/특정 원소들의 합/sum-of-specific-elements.cpp
+++ 240502/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -12,12 +12,12 @@ int main() {
 
     int sum = 0;
     for(int i=0; i<4; i++){
-        for(int j=0; j<4; j++){
-            sum += arr[i][j]
+        for(int j=0; j<i+1; j++){
+            sum += arr[i][j];
         }
     }
 
     cout << sum;
-    
+
     return 0;
 }
