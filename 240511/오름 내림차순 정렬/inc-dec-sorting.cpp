b771aaa56db096f73364f3052489e166868b3e31
limjihyee <83489228+limjihyee@users.noreply.github.com>
2024-05-11 16:28:20 +0900

    Title: 오름 내림차순 정렬, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#include <algorithm> // 오름차순 정렬을 위해 추가
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    // 오름차순
+    sort(arr, arr+n+1);
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+
+    // 내림차순
+    for(int i=n-1; i>=0; i--){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
